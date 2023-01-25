6d695bec9f848a58fa7241665c83135f7e34d73f
neweracy <kwakansah2@gmail.com>
2023-01-25 14:47:21 +0000

    index

--- index.js
+++ index.js
@@ -10,14 +10,23 @@
 const init = require('./utils/init');
 const cli = require('./utils/cli');
 const log = require('./utils/log');
+const axios = require("axios");
 
 const input = cli.input;
 const flags = cli.flags;
-const { clear, debug } = flags;
+const {
+	clear,
+	debug
+} = flags;
 
 (async () => {
-	init({ clear });
+	init({
+		clear
+	});
 	input.includes(`help`) && cli.showHelp(0);
-
 	debug && log(flags);
+	if (input.includes('joke')) {
+		const res = await axios.get('https://api.chucknorris.io/jokes/random')
+		console.log(res.data.value)
+	}
 })();
